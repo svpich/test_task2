9746162dc9f8ee9cf0ca36c5b272b60ede517eac
Svyatoslav Pichcar <sv.pich@mail.ru>
2022-01-06 20:18:04 +0900

    refactoring

--- src/main/app/api/impl/VK_ApiImpl.java
+++ src/main/app/api/impl/VK_ApiImpl.java
@@ -3,18 +3,24 @@ package app.api.impl;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.HttpMessageEncoder;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpUtils;
 
 @Component
 public class VK_ApiImpl implements app.api.abstracts.VK_Api {
 
-    @Autowired
     URIBuilder uriBuilder;
-
     final static String ACCESS_TOKEN = "d639528e12579c0998f65e0eae3294f9214c2743bb2b69653c933fcd51feca04b88b339e31703b7e15385";
     final static String PROTOCOL = "https";
     final static String HOST = "api.vk.com";
     final static String API_VERSION = "5.131";
+
+    @Autowired
+    public VK_ApiImpl (URIBuilder uriBuilder) {
+        this.uriBuilder = uriBuilder;
+    }
 
     @Override
     public HttpResponse findGroupBySubstring(String subString) {
